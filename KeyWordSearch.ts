3d1a28cc5766256d5fdcc0f60c1e349ff32d2d5f
sparsha <saha.sparsha@gmail.com>
2018-07-07 20:24:45 +0530

    KyeWordSearch added

--- KeyWordSearch.ts
+++ KeyWordSearch.ts
@@ -2,7 +2,7 @@ module Search {
     export class KeyWordSearch<T> implements ISearch<T> {
 
         public getSearchResults(objectsToSearchIn: T[], searchParameters: SearchParameter[]): T[] {
-            
+            return ;
         }
 
     }
