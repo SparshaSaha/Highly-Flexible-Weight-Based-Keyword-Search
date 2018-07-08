29cb7fd3229c1e3dd3cb7efa6d96f8650e36f8ec
sparsha <saha.sparsha@gmail.com>
2018-07-08 18:11:59 +0530

    Completed Keyword Search

--- KeyWordSearch.ts
+++ KeyWordSearch.ts
@@ -3,9 +3,12 @@ module Search {
 
         public getSearchResults(objectsToSearchIn: T[], searchParameters: SearchParameter[], query: string): T[] {
             let weightedObjects = this.generateWeightsForObjects(objectsToSearchIn, searchParameters, query);
-
+            return this.sortObjectsOnWeight(weightedObjects);
         }
 
+        // Generated weights for all objects
+        // Filters objects with weight equalto 0
+        // @returns weighted and filtered objects
         public generateWeightsForObjects(objectsToSearchIn: T[],searchParameters: SearchParameter[], query: string): ObjectWithWeight<T>[] {
             
             let objectsWithWeight: ObjectWithWeight<T>[] = []; 
@@ -25,11 +28,20 @@ module Search {
             return objectsWithWeight;
         }
 
+        // Function to sort objects on the descending order of weights of weights
+        // @returns unwrapped sorted objects
         public sortObjectsOnWeight(objectsWithWeight: ObjectWithWeight<T>[]): T[] {
-            let sorted = objectsWithWeight.sort(function(a, b) {
-                
+            let sortedObjects = objectsWithWeight.sort(function(a, b) {
+                return b.weight - a.weight;
             });
-        }
 
+            let unwrappedObjects: T[] = [];
+
+            for (let currentObject of sortedObjects) {
+                unwrappedObjects.push(currentObject.object);
+            }
+
+            return unwrappedObjects; 
+        }
     }
 }
