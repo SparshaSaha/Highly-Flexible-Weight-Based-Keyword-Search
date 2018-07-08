90404fc635ab69cb766e5cde0bb5182fd3c50df0
sparsha <saha.sparsha@gmail.com>
2018-07-09 00:35:13 +0530

    Lints fixed

--- ISearch.interface.ts
+++ ISearch.interface.ts
@@ -1,10 +1,10 @@
 import { SearchParameter } from "./SearchParameter";
-  export interface ISearch<T> {
-      /*
-      @param objectsToSearchIn: Takes in objects of generic type T on which search has been conducted
-      @param searchParameters: Parameters on which search has to be conducted specifically
-      @param query: String query to search for
-      @return Weight wise sorted objects
-      */
-      getSearchResults(objectsToSearchIn, searchParameters: SearchParameter[], query: string): T[];
-  }
+export interface ISearch<T> {
+  /*
+  @param objectsToSearchIn: Takes in objects of generic type T on which search has been conducted
+  @param searchParameters: Parameters on which search has to be conducted specifically
+  @param query: String query to search for
+  @return Weight wise sorted objects
+  */
+  getSearchResults(objectsToSearchIn, searchParameters: SearchParameter[], query: string): T[];
+}
--- KeyWordSearch.ts
+++ KeyWordSearch.ts
@@ -2,48 +2,48 @@ import { ObjectWithWeight } from "./ObjectWithWeight";
 import { SearchParameter } from "./SearchParameter";
 import { ISearch } from "./ISearch.interface";
 
-    export class KeyWordSearch<T> implements ISearch<T> {
+export class KeyWordSearch<T> implements ISearch<T> {
 
-        public  getSearchResults(objectsToSearchIn: T[], searchParameters: SearchParameter[], query: string): T[] {
-            let weightedObjects = this.generateWeightsForObjects(objectsToSearchIn, searchParameters, query.toLowerCase());
-            return this.sortObjectsOnWeight(weightedObjects);
-        }
-
-        // Generated weights for all objects
-        // Filters objects with weight equalto 0
-        // @returns weighted and filtered objects
-        private generateWeightsForObjects(objectsToSearchIn: T[],searchParameters: SearchParameter[], query: string): ObjectWithWeight<T>[] {
-            
-            let objectsWithWeight: ObjectWithWeight<T>[] = []; 
-            for(let currentObject of objectsToSearchIn) {
-                let weightForCurrentObject: number = 0;
-                for(let currentParameter of searchParameters) {
-                    if (currentObject[currentParameter.parameterName].toLowerCase().includes(query)) {
-                        weightForCurrentObject += currentParameter.parameterWeight;
-                    }
-                }
-
-                if (weightForCurrentObject > 0) {
-                    let objectWithWeight = new ObjectWithWeight<T>(currentObject, weightForCurrentObject);
-                    objectsWithWeight.push(objectWithWeight);
-                }
-            }
-            return objectsWithWeight;
-        }
-
-        // Function to sort objects on the descending order of weights of weights
-        // @returns unwrapped sorted objects
-        private sortObjectsOnWeight(objectsWithWeight: ObjectWithWeight<T>[]): T[] {
-            let sortedObjects = objectsWithWeight.sort(function(a, b) {
-                return b.weight - a.weight;
-            });
-
-            let unwrappedObjects = [];
-
-            for (let currentObject of sortedObjects) {
-                unwrappedObjects.push(currentObject.object);
-            }
-
-            return unwrappedObjects; 
-        }
+    public  getSearchResults(objectsToSearchIn: T[], searchParameters: SearchParameter[], query: string): T[] {
+        let weightedObjects = this.generateWeightsForObjects(objectsToSearchIn, searchParameters, query.toLowerCase());
+        return this.sortObjectsOnWeight(weightedObjects);
     }
+
+    // Generated weights for all objects
+    // Filters objects with weight equalto 0
+    // @returns weighted and filtered objects
+    private generateWeightsForObjects(objectsToSearchIn: T[],searchParameters: SearchParameter[], query: string): ObjectWithWeight<T>[] {
+            
+        let objectsWithWeight: ObjectWithWeight<T>[] = []; 
+        for(let currentObject of objectsToSearchIn) {
+            let weightForCurrentObject: number = 0;
+            for(let currentParameter of searchParameters) {
+                if (currentObject[currentParameter.parameterName].toLowerCase().includes(query)) {
+                    weightForCurrentObject += currentParameter.parameterWeight;
+                }
+            }
+
+            if (weightForCurrentObject > 0) {
+                let objectWithWeight = new ObjectWithWeight<T>(currentObject, weightForCurrentObject);
+                objectsWithWeight.push(objectWithWeight);
+            }
+        }
+        return objectsWithWeight;
+    }
+
+    // Function to sort objects on the descending order of weights of weights
+    // @returns unwrapped sorted objects
+    private sortObjectsOnWeight(objectsWithWeight: ObjectWithWeight<T>[]): T[] {
+        let sortedObjects = objectsWithWeight.sort(function(a, b) {
+            return b.weight - a.weight;
+        });
+
+        let unwrappedObjects = [];
+
+        for (let currentObject of sortedObjects) {
+            unwrappedObjects.push(currentObject.object);
+        }
+
+        return unwrappedObjects; 
+    }
+}
